6fe321026d7efbb25598665ca9ee89b743e3f86d
PolozovMichael <61011772+PolozovMichael@users.noreply.github.com>
2022-04-20 03:40:37 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+// Setting up view engine
 app.set('view engine', 'ejs');
-
+// Static files 
 app.use(express.static('public'));
 app.use(express.static('public/img'));
 app.use(express.static('public/css'));
