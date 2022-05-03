da9799aecc04224b509bbb94bb2476612e66209c
PolozovMichael <61011772+PolozovMichael@users.noreply.github.com>
2022-05-04 01:03:20 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
-// Setting up view engine
+
 app.set('view engine', 'ejs');
-// Static files 
+
 app.use(express.static('public'));
 app.use(express.static('public/img'));
 app.use(express.static('public/css'));
@@ -12,8 +12,41 @@ app.use(express.static('public/css'));
 app.use('/', require('./routes/root.js'));
 app.use('/login', require('./routes/login.js'));
 app.use('/register', require('./routes/register.js'));
-//problem is solved
+
+
+mongoose.connect('mongodb://localhost/user_db')
+    .then(() => console.log('Connected to a MongoDB...'))
+    .catch(err => console.error('Failed to connect to a MongoDB', err));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+async function createUser() {
+    const user = new User({
+        name : 'Michael',
+        password : '123456'
+    });
+
+    const rslt = await user.save();
+    console.log(rslt);
+}
+
+async function getUser() {
+    const rslt = await User
+        .find({name : 'Michael'})
+        .limit(20)
+        .sort({name : '1'})
+        .select({name : '1'})
+    console.log(rslt);
+}
+
+createUser();
+getUser();
 
 app.listen(PORT, () => {
-    console.log(`Running on localhost ${PORT}`);
+    console.log(`Running on localhost:${PORT}`);
 });
